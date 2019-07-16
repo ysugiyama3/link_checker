9b7c36ba4cc275e0c4994cfaa19b7cd35a0dbe19
Yukari Sugiyama <yukari.sugiyama@yale.edu>
2019-07-16 09:21:47 -0400

    add check_status.py generate_input_file.py sample_input_file.csv

---
+++ check_status.py
@@ -0,0 +1,187 @@
+#! python3
+# -*- coding: utf-8 -*-
+# Check status of links within catalog records
+import requests
+import csv
+import time
+import sqlite3
+import smtplib
+from os.path import basename
+from email.utils import formatdate
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+#===============================================================================
+# defs
+#===============================================================================
+       
+def check_url(u):
+# Check url u. Return HTTP response.   
+    try:
+        r = requests.head(u, allow_redirects=True, timeout=50)
+        code = r.status_code  
+    except (requests.HTTPError, requests.URLRequired, requests.exceptions.RequestException, requests.TooManyRedirects, requests.ConnectionError, requests.ConnectTimeout, requests.ReadTimeout, requests.exceptions.Timeout):
+        code = 'Error'
+    if not str(code).startswith('2'):
+        try:
+            r = requests.get(u, allow_redirects=True, timeout=50)
+            code = r.status_code           
+        except (requests.ConnectTimeout, requests.ReadTimeout, requests.exceptions.Timeout):
+            code = 'Timeout Error'              
+        except (requests.TooManyRedirects, requests.ConnectionError):
+            code = 'Connection Error' 
+        except (requests.HTTPError, requests.URLRequired, requests.exceptions.RequestException):
+            code = 'Other Error'                    
+    return code   
+  
+def send_email(attach_file, body, recipients, subject):
+# Send email with file attachment. 
+    sender = """'xyz@xxx.edu'"""
+    msg = MIMEMultipart()
+    msg['from'] = sender
+    msg['To'] = ','.join(recipients)
+    msg['date'] = formatdate(localtime=True)
+    msg['subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        file = attach_file
+        part = MIMEBase('application', "octet-stream")
+        with open(file, 'rb') as fh:
+            part.set_payload(fh.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment', \
+                                          filename=basename(file))
+            msg.attach(part)
+    except (ValueError, TypeError):
+        pass 
+    smtp = smtplib.SMTP('''ENTER_HOST''')
+    smtp.sendmail(sender, recipients, msg.as_string())
+    smtp.close()     
+
+#===============================================================================
+# variables
+#===============================================================================
+
+# list of reporting departments
+unit_list = ['''ENTER_LIST_OF_DEPTS''']
+'''
+For example
+unit_list = ['ART', 'DIV']
+'''
+
+# recipients' email addresses of each department
+recipients = dict('''ENTER_EMAIL_ADDRESSES''')
+'''
+For example
+recipients = dict(ART=['abc@xxx.edu'], DIV=['abc@xxx.edu','def@xxx.edu'])
+'''
+
+#===============================================================================
+# main
+#===============================================================================
+
+# Create and connect database
+conn = sqlite3.connect('''ENTER_DATABASE''')
+'''
+For example
+conn = sqlite3.connect('xxx.sqlite')
+'''
+cur = conn.cursor()		
+
+# Create table								
+cur.executescript('''
+CREATE TABLE IF NOT EXISTS Print (
+    ID  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    BibID  INTEGER,
+    Host    TEXT,
+    URL     TEXT,
+	Status		TEXT,
+    MFHD_Location   TEXT,
+	Check_Date  DATE,
+    Unit    TEXT
+);
+''')
+
+# Record start time
+start_time = time.time()
+
+# Select input csv file and generate output csv file
+input_name = input('Enter input name: ')
+output_name = input_name[:-4] + "_output.csv"
+
+# Select unit name to email
+unit = '' 
+while unit not in unit_list:
+    unit = input('Select unit name: ')
+
+# Email subject
+subject = 'Link Errors (' + unit + ')' 
+
+# Error counts    
+not_found = connection_error = timeout_error = other_error = 0
+
+# Open output csv file
+with open(output_name, 'w', newline='', encoding='utf-8-sig') as csvoutput:
+    csv_out = csv.writer(csvoutput, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_ALL)
+    csv_out.writerow(["ID","HOST","URL","STATUS","MFHD_LOC"])   
+       
+# Open input csv file 
+    rows = csv.reader(open(input_name, 'r', encoding='utf-8-sig'))    
+    num_lines = len(open(input_name).read().splitlines())
+    count = 0
+    
+# List of unique urls   
+    url_list = dict()
+
+# Check each url
+# If same link has not been checked yet, check its http status and update results to url_list
+# If same link has already been checked, get result from url_list   
+    for row in rows:
+        bib = str(row[0])
+        host = row[1]
+        url = row[2]
+        mfhd_loc = row[3]
+        if url not in url_list:
+            code = check_url(url)           
+            url_list.update({url:code})        
+        else:
+            code = url_list.get(url)                   
+        count += 1
+
+# Write results to console       
+        print (str(count) +  " of " + str(num_lines) + " | " + bib, code)
+
+# Write error results to output csv file
+        if str(code).startswith('400') or str(code).startswith('404'):
+            csv_out.writerow([bib, host, url, code, mfhd_loc])
+            not_found += 1
+        if str(code).startswith('Connection'):
+            csv_out.writerow([bib, host, url, code, mfhd_loc])
+            connection_error += 1
+        if str(code).startswith('Timeout'):
+            csv_out.writerow([bib, host, url, code, mfhd_loc])
+            timeout_error += 1 
+        if str(code).startswith('Other'):
+            csv_out.writerow([bib, host, url, code, mfhd_loc])
+            other_error += 1             
+
+# Write results to database                           
+        cur.execute('SELECT ID FROM Print WHERE BibID = ? AND URL = ?', (bib, url, )) 
+        fetch = cur.fetchone()
+        if fetch is None:
+            cur.execute('INSERT INTO Print (BibID, Host, URL, Status, MFHD_Location, Check_Date, Unit) VALUES ( ?, ?, ?, ?, ?, CURRENT_DATE, ? )', (bib, host, url, code, mfhd_loc, unit) )
+        else: 
+            cur.execute('UPDATE Print SET Status = ?, Check_Date = CURRENT_DATE, Unit = ? WHERE ID = ?', (code, unit, fetch[0],))
+        conn.commit()       
+csvoutput.close()
+cur.close()
+
+# If there is any error, email results
+text_body = 'Among ' + str(num_lines) + ' links' + '\n\n400/404 Error = ' + str(not_found) + '\nConnection Error = ' + str(connection_error) + '\nTimeout Error = ' + str(timeout_error) + '\nOther Error = ' + str(other_error)
+if not_found + connection_error + timeout_error + other_error > 0:        
+    send_email(output_name, text_body, recipients[unit], subject)
+print (text_body)
+print("\n%f seconds" % (time.time() - start_time))
---
+++ generate_input_file.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Generate a list of URLs per department/collection location
+# Reference: https://www.programcreek.com/python/example/94405/cx_Oracle.makedsn
+
+import cx_Oracle
+import csv
+import datetime
+
+#===============================================================================
+# defs
+#===============================================================================
+
+def split(filehandler):
+# Split csv into separate files containing 10,000 rows
+    csvfile = open(filehandler, 'r').readlines()
+    filename = 1
+    output_name = str(filehandler)[:-4] + '_'
+    for i in range(len(csvfile)):
+        if i % 10000 == 0:
+            open(output_name + str(filename) + '.csv', 'w+').writelines(csvfile[i:i+10000])
+            filename += 1
+
+#===============================================================================
+# variables
+#===============================================================================
+
+# List of reporting departments
+unit_list = ['''ENTER_LIST_OF_DEPTS''']
+'''
+For example
+unit_list = ['ART', 'DIV']
+'''
+
+# Query
+query = {'''ENTER_SQL'''}
+'''
+For example
+query = {
+'ART':"""                    
+    SELECT * FROM (                
+    SELECT
+     bib_master.bib_id,
+     elink_index.url_host,
+     elink_index.link,
+     location.location_code,
+     ROW_NUMBER() OVER (PARTITION BY bib_master.bib_id, elink_index.link ORDER BY bib_master.bib_id) RowNumber
+    FROM
+     elink_index
+     INNER JOIN bib_master ON elink_index.record_id = bib_master.bib_id
+     INNER JOIN bib_mfhd ON bib_master.bib_id = bib_mfhd.bib_id
+     INNER JOIN mfhd_master ON bib_mfhd.mfhd_id = mfhd_master.mfhd_id
+     INNER JOIN location ON mfhd_master.location_id = location.location_id
+    WHERE
+     bib_master.suppress_in_opac = 'N'
+     AND elink_index.record_type = 'B'
+     AND mfhd_master.suppress_in_opac = 'N'
+     AND mfhd_master.display_call_no NOT LIKE '%UNCAT%'
+     AND mfhd_master.display_call_no NOT LIKE '%On Order%'
+     AND mfhd_master.display_call_no NOT LIKE '%In Process%'
+     AND location.location_id IN (
+         '1',
+         '2',
+         '3'
+     )) a
+     WHERE RowNumber = 1
+    """,
+'DIV':"""                    
+    SELECT * FROM (                
+    SELECT
+     bib_master.bib_id,
+     elink_index.url_host,
+     elink_index.link,
+     location.location_code,
+     ROW_NUMBER() OVER (PARTITION BY bib_master.bib_id, elink_index.link ORDER BY bib_master.bib_id) RowNumber
+    FROM
+     elink_index
+     INNER JOIN bib_master ON elink_index.record_id = bib_master.bib_id
+     INNER JOIN bib_mfhd ON bib_master.bib_id = bib_mfhd.bib_id
+     INNER JOIN mfhd_master ON bib_mfhd.mfhd_id = mfhd_master.mfhd_id
+     INNER JOIN location ON mfhd_master.location_id = location.location_id
+    WHERE
+     bib_master.suppress_in_opac = 'N'
+     AND elink_index.record_type = 'B'
+     AND mfhd_master.suppress_in_opac = 'N'
+     AND mfhd_master.display_call_no NOT LIKE '%UNCAT%'
+     AND mfhd_master.display_call_no NOT LIKE '%On Order%'
+     AND mfhd_master.display_call_no NOT LIKE '%In Process%'
+     AND location.location_id IN (
+         '4',
+         '5',
+         '6'
+     )) a
+     WHERE RowNumber = 1
+    """}
+'''
+
+#===============================================================================
+# main
+#===============================================================================
+
+# Make connection to the oracle database
+dsn = cx_Oracle.makedsn('''ENTER_HOST,ENTER_PORT,ENTER_DATABASE NAME''') # taken from tnsnames.ora
+con = cx_Oracle.connect(user='''ENTER_USERNAME''',password='''ENTER_PASSWORD''',dsn=dsn)
+cur = con.cursor()
+
+# Select department/collection location
+unit = '' 
+while unit not in unit_list:
+    unit = input ('Enter unit name: ')
+
+# Execute query
+cur.execute(query[unit])
+r = cur.fetchall()
+cur.close()
+con.close()
+
+now = datetime.datetime.now()
+
+# Create output csv file
+output_name = unit + '_' + now.strftime('%Y%m%d') + '.csv'
+
+# Write query results to output csv file    
+with open(output_name, 'w', newline='', encoding='utf-8-sig') as outfile:
+    writer = csv.writer(outfile)
+    c = 0
+    for row in r:
+        id = str(row[0])
+        url = row[1]	
+        host = row[2]
+        loc = row[3]
+        writer.writerow((id,url,host,loc))
+        c += 1	
+    print('links in ELINK_INDEX: ', c)
+outfile.close()
+
+# Split csv into separate files containing 10,000 rows
+if c > 10000:
+    split(output_name)
